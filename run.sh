c0d6e0cb6b99fd921b27335f36bdd11f4194f485
pencilCool <yhtangcoder@gmail.com>
2020-07-19 22:22:59 +0800

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-# clang -S -emit-llvm hello.c -o hello2.ll
 
-clang -fmodules -fsyntax-only -Xclang -ast-dump elseif.c
-clang -fmodules -fsyntax-only -Xclang -ast-dump if.c
+clang -fmodules -fsyntax-only -Xclang -ast-dump elseif.c > elseif.ast
+clang -fmodules -fsyntax-only -Xclang -ast-dump if.c > if.ast
